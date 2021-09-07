11cfc030b3ed170cfa667dc40de788c283b72a63
andron13 <andron13@gmail.com>
2021-09-07 17:55:56 +0200

    fix: correcte a few scripts :ambulance:

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,19 +1,30 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+export default function createDreamTeam(members) {
+    if ( Array.isArray(members) !== true) {
+        return false;
+    }
+    let result = "";
+    for (let item of members) {
+        if (typeof item === "string") {
+            result += item.trim().substring(0, 1).toUpperCase();
+        }
+    }
+    return result.split("").sort().join("");
 }
+
+
